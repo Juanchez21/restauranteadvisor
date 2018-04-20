42d984719da88ec88341eb889f587f4e4c4cfdf3
Juan Sánchez <jusanc06@ucm.es>
2018-04-20 12:06:22 +0200

    Beuh

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head> <title>Index</title>
+	<meta charset = "UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+	<?php require ('cabecera.php');?>
+	<div class="cuerpo">
+		<?php require ('menu_usuario.php');?>
+		<div class="portada">
+			<h1>Página principal</h1>
+			<p> Aquí está la portada, que ya la haremos </p>
+		</div>
+	</div>
+</body>
+</html>
