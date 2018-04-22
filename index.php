7e14e3993b8d4732dedc48cc2bfd81c4121e9313
Eliana Aliz <elianaaliz16@gmail.com>
2018-04-22 15:07:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,41 @@
 		<div class="portada">
 			<h1>Página principal</h1>
 			<p> Aquí está la portada, que ya la haremos </p>
+			<h1>The position Property</h1>
+
+			<h2>This is a heading with an absolute position</h2>
+
+			<p>With absolute positioning, an element can be placed anywhere on a page. The heading below is placed 100px from the left of the page and 150px from the top of the page.</p>
+			<h1>The position Property</h1>
+
+			<h2>This is a heading with an absolute position</h2>
+
+			<p>With absolute positioning, an element can be placed anywhere on a page. The heading below is placed 100px from the left of the page and 150px from the top of the page.</p>
+			<h1>The position Property</h1>
+
+			<h2>This is a heading with an absolute position</h2>
+
+			<p>With absolute positioning, an element can be placed anywhere on a page. The heading below is placed 100px from the left of the page and 150px from the top of the page.</p>
+			<h1>The position Property</h1>
+
+			<h2>This is a heading with an absolute position</h2>
+
+			<p>With absolute positioning, an element can be placed anywhere on a page. The heading below is placed 100px from the left of the page and 150px from the top of the page.</p>
+			<h1>The position Property</h1>
+
+			<h2>This is a heading with an absolute position</h2>
+
+			<p>With absolute positioning, an element can be placed anywhere on a page. The heading below is placed 100px from the left of the page and 150px from the top of the page.</p>
+			<h1>The position Property</h1>
+
+			<h2>This is a heading with an absolute position</h2>
+
+			<p>With absolute positioning, an element can be placed anywhere on a page. The heading below is placed 100px from the left of the page and 150px from the top of the page.</p>
+			<h1>The position Property</h1>
+
+			<h2>This is a heading with an absolute position</h2>
+
+			<p>With absolute positioning, an element can be placed anywhere on a page. The heading below is placed 100px from the left of the page and 150px from the top of the page.</p>
 		</div>
 	</div>
 </body>
