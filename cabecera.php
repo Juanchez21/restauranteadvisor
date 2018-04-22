663e20df490f3eefc6fd7c4a5f920814383a8db1
Eliana Aliz <elianaaliz16@gmail.com>
2018-04-22 15:06:18 +0200

    Update cabecera.php

--- cabecera.php
+++ cabecera.php
@@ -1,7 +1,22 @@
 <div class="cabecera">
 	<ul class="head-menu">
 		<li>
+			<a class="vertical-centered plain-link white-link" href="index.php">LOGO</a>
+		</li>
+		<li>
 			<a class="vertical-centered plain-link white-link" href="index.php">Inicio</a>
+		</li>
+		<li>
+			<a class="vertical-centered plain-link white-link" href="contacto.php">Cenas</a>
+		</li>
+		<li>
+			<a class="vertical-centered plain-link white-link" href="contacto.php">Comidas</a>
+		</li>
+		<li>
+			<a class="vertical-centered plain-link white-link" href="contacto.php">Desayuno</a>
+		</li>
+		<li>
+			<a class="vertical-centered plain-link white-link" href="contacto.php">Restaurantes Baratos</a>
 		</li>
 		<li>
 			<a class="vertical-centered plain-link white-link" href="contacto.php">Contacto</a>
